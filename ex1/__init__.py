574acbe7c0a24a24c414edc689b73b4efa715be2
erraii <eraycakiray@gmail.com>
2026-06-20 02:00:26 +0200

    Refactor imports in __init__.py for clarity

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -1,5 +1,10 @@
-from .transform_creature_factory import HealingCreatureFactory
+from .transform_creature_factory import (
+    HealingCreatureFactory,
+    TransformCreatureFactory,
+)
+
 
 __all__ = [
     "HealingCreatureFactory",
+    "TransformCreatureFactory",
 ]
