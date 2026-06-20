6d9e288e9618837ffe95dc54e34a4ef638d2ced1
erraii <eraycakiray@gmail.com>
2026-06-20 02:00:06 +0200

    Refactor main to test Healing and Transform creatures

--- capacitor.py
+++ capacitor.py
@@ -2,18 +2,36 @@ import ex1
 
 
 def main() -> None:
+
     print("Testing Creature with healing capability")
     print(" base:")
-    factory = ex1.HealingCreatureFactory()
-    base_creature = factory.create_base()
-    print(base_creature.describe())
-    print(base_creature.attack())
-    print(base_creature.heal("itself"))
+    factoryHeal = ex1.HealingCreatureFactory()
+    base_creature1 = factoryHeal.create_base()
+    print(base_creature1.describe())
+    print(base_creature1.attack())
+    print(base_creature1.heal("itself"))
     print(" evolved:")
-    evolved_creature = factory.create_evolved()
-    print(evolved_creature.describe())
-    print(evolved_creature.attack())
-    print(evolved_creature.heal("itself and others"))
+    evolved_creature1 = factoryHeal.create_evolved()
+    print(evolved_creature1.describe())
+    print(evolved_creature1.attack())
+    print(evolved_creature1.heal("itself and others"))
+
+    print("\nTesting Creature with healing capability")
+    print(" base:")
+    factoryTrans = ex1.TransformCreatureFactory()
+    base_creature2 = factoryTrans.create_base()
+    print(base_creature2.describe())
+    print(base_creature2.attack())
+    print(base_creature2.transform())
+    print(base_creature2.attack())
+    print(base_creature2.revert())
+    print(" evolved:")
+    evolved_creature2 = factoryTrans.create_evolved()
+    print(evolved_creature2.describe())
+    print(evolved_creature2.attack())
+    print(evolved_creature2.transform())
+    print(evolved_creature2.attack())
+    print(evolved_creature2.revert())
 
 
 if __name__ == "__main__":
