837004b27bfa877cfda15e5b715720307dd25309
erraii <eraycakiray@gmail.com>
2026-06-21 14:37:42 +0200

    Update __init__.py

--- ex2/__init__.py
+++ ex2/__init__.py
@@ -1,1 +1,12 @@
+from ex2.battle_strategy import (
+    NormalStrategy,
+    AggressiveStrategy,
+    DefensiveStrategy,
+)
 
+
+__all__ = [
+    "NormalStrategy",
+    "AggressiveStrategy",
+    "DefensiveStrategy",
+]
